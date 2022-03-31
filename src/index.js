ca62c55cbd98ad02a90d11ed9449366dc8651e1f
Sarai Ramadan <sramadan7@Sarais-MacBook-Air.local>
2022-03-30 19:33:15 -0600

    Adding react styling for rounded image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import React, { Component } from "react";
+import ReactRoundedImage from "react-rounded-image";
+import MyPhoto from "./src/assets/profile.jpg";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
